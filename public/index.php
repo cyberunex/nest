66195b38a4d7ffb6165ff75c19f6e86384c6edde
cyberunex <runex@live.cn>
2016-10-04 16:51:08 +0800

    create program‘s entry
    add Buddha comment;

---
+++ public/index.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ *
+ *                                  _oo8oo_
+ *                                 o8888888o
+ *                                 88" . "88
+ *                                 (| -_- |)
+ *                                 0\  =  /0
+ *                               ___/'==='\___
+ *                             .' \\|     |// '.
+ *                            / \\|||  :  |||// \
+ *                           / _||||| -:- |||||_ \
+ *                          |   | \\\  -  /// |   |
+ *                          | \_|  ''\---/''  |_/ |
+ *                          \  .-\__  '-'  __/-.  /
+ *                        ___'. .'  /--.--\  '. .'___
+ *                     ."" '<  '.___\_<|>_/___.'  >' "".
+ *                    | | :  `- \`.:`\ _ /`:.`/ -`  : | |
+ *                    \  \ `-.   \_ __\ /__ _/   .-` /  /
+ *                =====`-.____`.___ \_____/ ___.`____.-`=====
+ *                                  `=---=`
+ *               ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ *
+ *                          Buddha bless, No bug...
+ * 
+ *  The author disclaims copyright to this source code.  In place of a legal notice, here is a blessing:
+ *  May you do good and not evil.
+ *  May you find forgiveness for yourself and forgive others.
+ *  May you share freely, never taking more than you give.
+ *
+ */
+ 
